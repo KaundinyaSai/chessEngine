4bc73e440473dba4229d7058aee249c844e98c12
kaundinya <kaundinya.kautilya@gmail.com>
2025-06-22 10:26:38 +0530

    organization

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,11 +3,11 @@ using System.Diagnostics;
 
 Magic.AttackTablesInit();
 
-GameState game = new GameState("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ");
+GameState game = new GameState();
 
 Stopwatch stopwatch = Stopwatch.StartNew();
 
-Search.PerftDivide(game, 4);
+Search.PerftDivide(game, 5);
 
 stopwatch.Stop();
 Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
